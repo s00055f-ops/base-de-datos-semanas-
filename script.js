ba6e94eda41c09950dcea83d3dae6655f3b4cc51
s00055f-ops <s00055f@upla.edu.pe>
2025-10-01 21:00:03 -0500

    Update script.js
    Signed-off-by: s00055f-ops <s00055f@upla.edu.pe>

--- script.js
+++ script.js
@@ -9,6 +9,8 @@
                 links: [
                     { url: "https://www.canva.com/design/DAGzMAgwKcs/cDgWGMPUGi-ReOzBb-1Q4g/edit?utm_content=DAGzMAgwKcs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton", texto: "📊 Informe Técnico Comparativo: Arquitecturas Centralizadas, Cliente- Servidor y Distribuidas" },
                     { url: "https://docs.google.com/document/d/1iA9bNV7w68wZU6t-g1Qa_psu4yUDOXWVNM9rRp6vuK4/edit?usp=sharing", texto: "📄 Manual: Creación de una cuenta en GitHub" },
+                  { url: "https://docs.google.com/document/d/1SW5tpWgkpjzzfOfTWFI2-Y7z0oStLC_JaKF6Mhj9Hcw/edit?usp=sharing", texto: "📄 MANUAL PARA SUBIR LA PAGINA WEB A GIT HUB" },
+              
               
                   
                 ] 
